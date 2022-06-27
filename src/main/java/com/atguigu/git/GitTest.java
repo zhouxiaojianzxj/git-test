76fa9521178568ccb00d37de7cf934e323b148a6
zxj <zxj@qq.com>
2022-06-27 19:39:30 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zhouxiaojian
+ * @date 2022-06-27 19:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
